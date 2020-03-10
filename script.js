5f766950f3466ec9d24adb21fe87921773b857ea
rayfungray <61134260+rayfungray@users.noreply.github.com>
2020-03-10 17:52:36 -0600

    added class

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 document.title = "FizzBuzz";
-var fizz = document.getElementsByTagName('body');
+// var fizz = document.getElementsByTagName('body');
+var body = document.body;
 
 var addContainer = document.createElement('div');
 addContainer.classList.add('container');
-fizz[0].appendChild(addContainer);
+body.appendChild(addContainer);
 
 
 for(var i = 1; i < 101; i++){
 var addDiv = document.createElement('div');
-  addDiv.classList.add('element');
+  addDiv.classList.add('circle');
   if (i % 15 === 0)
 {
+  addDiv.classList.add('FizzBuzz');
   addDiv.innerHTML = "FizzBuzz";
+  
 } else if (i % 5 === 0)
 {
+  addDiv.classList.add('buzz');
   addDiv.innerHTML = "Buzz";
   } else if (i % 3 === 0)
 {
+  addDiv.classList.add('Fizz');
   addDiv.innerHTML = "Fizz";
 }
 else
